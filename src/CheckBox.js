75db58d54194e03a9b330f5a49478fb69c9cf1ed
FBudn <111660524+FBudn@users.noreply.github.com>
2022-12-20 18:24:59 +0100

    deleted test div

--- src/CheckBox.js
+++ src/CheckBox.js
@@ -4,7 +4,6 @@ const CheckBox = () => {
     <div className="flex justify-start align-center items-center text-center gap-3 h-10 pl-1 -mt-2">
       <input type="checkbox" name="rememberPassword" id="rememberPasswordID" />
       <div className="text-sm font-medium">Remember me?</div>
-      <div>hej + </div>
     </div>
   );
 };
